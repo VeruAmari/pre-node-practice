4d3e2edbdd01090f8c2a9b708a69173bcf3d709a
Veru <43966969+VeruAmari@users.noreply.github.com>
2024-04-13 20:34:44 -0300

    feat: implement simple context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-// import { useState } from 'react'
+import { useContext, useState } from 'react';
+import { KissesContext } from './Contexts';
 import './styles/App.css';
 
 function App() {
+  const [kisses, setKisses] = useState(0);
+  const increase = () => {
+    setKisses((c) => c + 1);
+  };
+
   return (
     <>
-      <h1>Apply Concepts Here</h1>
+      <KissesContext.Provider value={kisses}>
+        <h1>Kissing Booth</h1>
+        <TotalKisses />
+        <button onClick={increase}>Give Kiss</button>
+      </KissesContext.Provider>
     </>
   );
 }
+
+const TotalKisses = () => {
+  const kisses = useContext(KissesContext);
+  return <p>{kisses}</p>;
+};
 
 export default App;
