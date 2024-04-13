df0b76505af9474909993b6b293043f503103a1b
Vero <43966969+VeruAmari@users.noreply.github.com>
2024-04-13 09:51:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Practice Repo
+
+The purpose of this repo is to practice applying Context API, useReducer and Refs and Memoization.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Apply Concepts Here</h1>
+    </>
+  );
+}
+
+export default App;
